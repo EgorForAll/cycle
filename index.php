70fb1069b1a32f6b568a5e9ca444e78bc09da21e
EgorForAll <eia.web.ss@gmail.com>
2024-03-31 09:09:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+$count = 0;
+function cycle($arr)
+{
+    global $count;
+    echo $arr[$count];
+    $count++;
+    if ($count < count($arr)) {
+        cycle($arr);
+    }
+}
+
+cycle($array);
+
